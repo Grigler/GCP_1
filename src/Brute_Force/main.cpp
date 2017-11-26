3b79965357a9c34968e82cbc1e874b7a50252965
Grigler <graham.rigler@gmail.com>
2017-11-26 20:38:02 +0000

    Finished report and added more arg customisability

--- src/Brute_Force/main.cpp
+++ src/Brute_Force/main.cpp
@@ -23,6 +23,15 @@
 #define MAX_Z UNIFORM_VAL
 #define MODEL_SCALE 25.0f
 
+//Globals - used for command-line args
+bool g_visualDrawing = false;
+bool g_isOffsetting = false;
+float g_modelScale = 75.0f;
+std::string g_inputFilePath = "./models/gourd.obj";
+//Used for dumping all hits - only instantiated with flag
+//uses NULL flag for conditional writing
+FILE *g_hitsDump = NULL;
+
 //Simple struct to holds the 3 vertex points
 //and the normal of the triangle
 struct Tri
@@ -114,23 +123,26 @@ struct Obj
       }
     }
     //Scaling+offsetting the mins and maxs TODO - do per xyz component
-    min = min * MODEL_SCALE + (UNIFORM_VAL / 2.0f);
-    max = max * MODEL_SCALE + (UNIFORM_VAL / 2.0f);
+    min = min * g_modelScale;// +(UNIFORM_VAL / 2.0f);
+    max = max * g_modelScale;// +(UNIFORM_VAL / 2.0f);
+    if (g_isOffsetting)
+    {
+      min += Hoops::Vec3(MAX_X*0.5f, MAX_Y*0.5f, MAX_Z*0.5f);
+      max += Hoops::Vec3(MAX_X*0.5f, MAX_Y*0.5f, MAX_Z*0.5f);
+    }
   }
 };
 
 void ParseArgs(int argc, char **argv);
+void BreakForInvalidArg();
 void DumpData(std::stringstream &_ss, std::string _path);
 
 void ExportImage(std::vector<unsigned char> &_raw, std::string _name);
 void ScrubRawImage(std::vector<unsigned char> &_raw);
-//Takes vector representation of a raw image (assumes correct size)
-//and populates with
+
 void BruteForceTrace(std::vector<unsigned char> &_imageVec, Obj *_obj);
 void BVTrace(std::vector<unsigned char> &_imageVec, Obj *_obj);
-
-bool g_visualDrawing = false;
-std::string g_inputFilePath = "./models/typhoon.obj";
+void TriBVTrace(std::vector<unsigned char> &_imageVec, Obj *_obj);
 
 int main(int argc, char **argv)
 {
@@ -138,6 +150,11 @@ int main(int argc, char **argv)
   if (argc > 1)
   {
     ParseArgs(argc, argv);
+  }
+  else if (argc == 0)
+  {
+    printf("Running default for gaurd.obj with visual output\n");
+    g_visualDrawing = true;
   }
 
   //Load objects here
@@ -155,7 +172,6 @@ int main(int argc, char **argv)
   /********************************************
   *               Brute Force                 *
   *********************************************/
-
   printf("BF:\n");
   dataCSV << "Iteration,run-time(ms)" << std::endl;
   for (size_t i = 0; i < 1; i++)
@@ -180,17 +196,23 @@ int main(int argc, char **argv)
   DumpData(dataCSV, "Brute_Force_Times.csv");
   dataCSV.str("");
 
+  if (g_hitsDump)
+  {
+    fclose(g_hitsDump);
+    g_hitsDump = fopen("hitsBV.csv", "w");
+    if(g_hitsDump)
+      fprintf(g_hitsDump, "rayX,rayY,rayZ,hitX,hitY,hitZ\n");
+  }
 
   /********************************************
   *              Bounding Volume              *
   *********************************************/
-
   //Done as a pre-comuputation
   object.BuildBV();
 
   printf("BV:\n");
   dataCSV << "Iteration,run-time(ms)" << std::endl;
-  for (size_t i = 0; i < 0; i++)
+  for (size_t i = 0; i < 1; i++)
   {
     printf("\t#%i\n", i);
     //Time Start
@@ -212,10 +234,17 @@ int main(int argc, char **argv)
   DumpData(dataCSV, "Bounding_Volume_Times.csv");
   dataCSV.str("");
 
+  if (g_hitsDump)
+  {
+    fclose(g_hitsDump);
+    g_hitsDump = fopen("hitsTriBV.csv", "w");
+    if (g_hitsDump)
+      fprintf(g_hitsDump, "rayX,rayY,rayZ,hitX,hitY,hitZ\n");
+  }
+
   /********************************************
   *               Triangle BV                 *
   *********************************************/
-
   printf("TriBV:\n");
   dataCSV << "Iteration,run-time(ms)" << std::endl;
   for (size_t i = 0; i < 1; i++)
@@ -248,6 +277,10 @@ void ParseArgs(int argc, char **argv)
   std::vector<std::string> validSwitches;
   validSwitches.push_back("--visual");
   validSwitches.push_back("--input");
+  validSwitches.push_back("--offset");
+  validSwitches.push_back("--scale");
+  validSwitches.push_back("--dump");
+  validSwitches.push_back("--help");
 
   bool validUse = false;
 
@@ -255,43 +288,90 @@ void ParseArgs(int argc, char **argv)
   for (size_t i = 1; i < argc; i++)
   {
     std::string arg = argv[i];
+    validUse = false;
     for (auto j = 0; j < validSwitches.size(); j++)
     {
       if (arg == validSwitches.at(j))
       {
+        validUse = true;
+        //NOTE - All these double string checks could be hard-coded
+        //by their array value of j, but that would be less read-able
+        //and more 'hacky'
+
+        //HELP FLAG
+        if (validSwitches.at(j) == "--help")
+        {
+          //It prints all the info anyway
+          BreakForInvalidArg();
+        }
+        //DRAWING RENDER WITH LODEPNG
         if (validSwitches.at(j) == "--visual")
         {
-          //Making folders as lodePNG cannot create them
+          //Making folders as lodePNG cannot export to
+          //a non-existent folder
           system("mkdir BF");
           system("mkdir BV");
           system("mkdir TriBV");
 
           g_visualDrawing = true;
-          validUse = true;
           break;
         }
-
+        //OBJ MODEL INPUT SWITCH
         if (validSwitches.at(j) == "--input")
         {
           if (argc > i)
           {
             g_inputFilePath = argv[i + 1];
-            validUse = true;
             break;
           }
           else
           {
-            validUse = false;
+            BreakForInvalidArg();
           }
         }
-      }
-    }
+        //OFFSETTING SWITCH
+        if (validSwitches.at(j) == "--offset")
+        {
+          g_isOffsetting = true;
+          break;
+        }
+        //SCALE SWITCH
+        if (validSwitches.at(j) == "--scale")
+        {
+          if (argc > i)
+          {
+            g_modelScale = atof(argv[i + 1]);
+          }
+          else
+          {
+            BreakForInvalidArg();
+          }
+        }
+        //DUMP HIT INFO SWITCH
+        if (validSwitches.at(j) == "--dump")
+        {
+          g_hitsDump = fopen("hitsBF.csv", "w");
+          if (g_hitsDump)
+            fprintf(g_hitsDump, "rayX,rayY,rayZ,hitX,hitY,hitZ\n");
 
-    if (!validUse)
-    {
-      std::cerr << "Usage:\n\tRayTrace [switches]\n\tValidSwitches:\n\t\t--visual - Outputs pngs of traces\n\t\t--input [path] uses input obj file to trace\n";
+          break;
+        }
+      }
+
     }
+    if (!validUse) BreakForInvalidArg();
   }
+}
+void BreakForInvalidArg()
+{
+  std::cerr <<
+    "Usage:\n\tRayTrace [switches]\n\tValidSwitches:\
+\n\t\t--visual : Outputs pngs of traces\
+\n\t\t--input [path] : uses input obj file to trace\
+\n\t\t--offset : attempts to offset object to center of screen by adding 0.5*image_size to each axis\
+\n\t\t--scale [val] : scales the model's verts by val\
+\n\t\t--dump : dumps every hit to hits.csv\n";
+  exit(1);
 }
 void DumpData(std::stringstream &_ss, std::string _path)
 {
@@ -339,9 +419,13 @@ void BruteForceTrace(std::vector<unsigned char> &_imageVec, Obj *_obj)
       for (size_t v = 0; v < fv; v++)
       {
         tinyobj::index_t i = _obj->shapes.at(s).mesh.indices.at(indexOffset + v);
-        tri.v[v].x = MODEL_SCALE * _obj->attrib.vertices.at(3 * i.vertex_index + 0) + (MAX_X / 2.0f);
-        tri.v[v].y = MODEL_SCALE * _obj->attrib.vertices.at(3 * i.vertex_index + 1) + (MAX_Y / 2.0f);
-        tri.v[v].z = MODEL_SCALE * _obj->attrib.vertices.at(3 * i.vertex_index + 2) + (MAX_Z / 2.0f);
+        tri.v[v].x = g_modelScale * _obj->attrib.vertices.at(3 * i.vertex_index + 0);// +(MAX_X / 2.0f);
+        tri.v[v].y = g_modelScale * _obj->attrib.vertices.at(3 * i.vertex_index + 1);// +(MAX_Y / 2.0f);
+        tri.v[v].z = g_modelScale * _obj->attrib.vertices.at(3 * i.vertex_index + 2);// +(MAX_Z / 2.0f);
+        if (g_isOffsetting)
+        {
+          tri.v[v] += Hoops::Vec3(MAX_X*0.5f, MAX_Y*0.5f, MAX_Z*0.5f);
+        }
       }
       //Calculating normal once per-triangle
       //rather than once per-ray
@@ -359,21 +443,16 @@ void BruteForceTrace(std::vector<unsigned char> &_imageVec, Obj *_obj)
           if (isHit && hitPoint.x < MAX_X && hitPoint.y < MAX_Y && hitPoint.z < MAX_Z
                     && hitPoint.x >= 0  && hitPoint.y >= 0  && hitPoint.z >= 0)
           {
+            if (g_hitsDump) fprintf(g_hitsDump, "0,%i,%i,%f,%f,%f\n", y, x, hitPoint.x, hitPoint.y, hitPoint.z);
+
             if (g_visualDrawing)
             {
               float facingRatio = Hoops::clamp(-Hoops::Dot(CONST_DIR, tri.n), 0.4f, 1.0f) * 0.5f;
 
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 0] = facingRatio * 128;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 1] = facingRatio * 255;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 2] = facingRatio * 128;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 3] = 255;
-            }
-            else
-            {
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 0] = 255;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 1] = 255;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 2] = 255;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 3] = 255;
+              _imageVec[Hoops::clamp((MAX_Y - y) *(MAX_Y * 4) + 4 * x, 0, (MAX_X*MAX_Y * 4) - 4) + 0] = facingRatio * 128;
+              _imageVec[Hoops::clamp((MAX_Y - y) *(MAX_Y * 4) + 4 * x, 0, (MAX_X*MAX_Y * 4) - 4) + 1] = facingRatio * 255;
+              _imageVec[Hoops::clamp((MAX_Y - y) *(MAX_Y * 4) + 4 * x, 0, (MAX_X*MAX_Y * 4) - 4) + 2] = facingRatio * 128;
+              _imageVec[Hoops::clamp((MAX_Y - y) *(MAX_Y * 4) + 4 * x, 0, (MAX_X*MAX_Y * 4) - 4) + 3] = 255;
             }
           }
         }
@@ -396,9 +475,13 @@ void BVTrace(std::vector<unsigned char> &_imageVec, Obj *_obj)
       for (size_t v = 0; v < fv; v++)
       {
         tinyobj::index_t i = _obj->shapes.at(s).mesh.indices.at(indexOffset + v);
-        tri.v[v].x = MODEL_SCALE * _obj->attrib.vertices.at(3 * i.vertex_index + 0) + (MAX_X / 2.0f);
-        tri.v[v].y = MODEL_SCALE * _obj->attrib.vertices.at(3 * i.vertex_index + 1) + (MAX_Y / 2.0f);
-        tri.v[v].z = MODEL_SCALE * _obj->attrib.vertices.at(3 * i.vertex_index + 2) + (MAX_Z / 2.0f);
+        tri.v[v].x = g_modelScale * _obj->attrib.vertices.at(3 * i.vertex_index + 0);// +(MAX_X / 2.0f);
+        tri.v[v].y = g_modelScale * _obj->attrib.vertices.at(3 * i.vertex_index + 1);// +(MAX_Y / 2.0f);
+        tri.v[v].z = g_modelScale * _obj->attrib.vertices.at(3 * i.vertex_index + 2);// +(MAX_Z / 2.0f);
+        if (g_isOffsetting)
+        {
+          tri.v[v] += Hoops::Vec3(MAX_X*0.5f, MAX_Y*0.5f, MAX_Z*0.5f);
+        }
       }
       //Calculating normal once per-triangle
       //rather than once per-ray
@@ -421,22 +504,17 @@ void BVTrace(std::vector<unsigned char> &_imageVec, Obj *_obj)
           if (isHit && hitPoint.x < MAX_X && hitPoint.y < MAX_Y && hitPoint.z < MAX_Z
                     && hitPoint.x >= 0  && hitPoint.y >= 0  && hitPoint.z >= 0)
           {
+            if (g_hitsDump) fprintf(g_hitsDump, "0,%i,%i,%f,%f,%f\n", y, x, hitPoint.x, hitPoint.y, hitPoint.z);
+
             if (g_visualDrawing)
             {
               float facingRatio = Hoops::clamp(-Hoops::Dot(CONST_DIR, tri.n), 0.4f, 1.0f) * 0.5f;
 
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 0] = facingRatio * 128;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 1] = facingRatio * 255;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 2] = facingRatio * 128;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 3] = 255;
-            }
-            else
-            {
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 0] = 255;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 1] = 255;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 2] = 255;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 3] = 255;
-            }            
+              _imageVec[Hoops::clamp((MAX_Y - y) *(MAX_Y * 4) + 4 * x, 0, (MAX_X*MAX_Y * 4) - 4) + 0] = facingRatio * 128;
+              _imageVec[Hoops::clamp((MAX_Y - y) *(MAX_Y * 4) + 4 * x, 0, (MAX_X*MAX_Y * 4) - 4) + 1] = facingRatio * 255;
+              _imageVec[Hoops::clamp((MAX_Y - y) *(MAX_Y * 4) + 4 * x, 0, (MAX_X*MAX_Y * 4) - 4) + 2] = facingRatio * 128;
+              _imageVec[Hoops::clamp((MAX_Y - y) *(MAX_Y * 4) + 4 * x, 0, (MAX_X*MAX_Y * 4) - 4) + 3] = 255;
+            }      
           }
         }
       }
@@ -458,9 +536,13 @@ void TriBVTrace(std::vector<unsigned char> &_imageVec, Obj *_obj)
       for (size_t v = 0; v < fv; v++)
       {
         tinyobj::index_t i = _obj->shapes.at(s).mesh.indices.at(indexOffset + v);
-        tri.v[v].x = MODEL_SCALE * _obj->attrib.vertices.at(3 * i.vertex_index + 0) + (MAX_X / 2.0f);
-        tri.v[v].y = MODEL_SCALE * _obj->attrib.vertices.at(3 * i.vertex_index + 1) + (MAX_Y / 2.0f);
-        tri.v[v].z = MODEL_SCALE * _obj->attrib.vertices.at(3 * i.vertex_index + 2) + (MAX_Z / 2.0f);
+        tri.v[v].x = g_modelScale * _obj->attrib.vertices.at(3 * i.vertex_index + 0);// +(MAX_X / 2.0f);
+        tri.v[v].y = g_modelScale * _obj->attrib.vertices.at(3 * i.vertex_index + 1);// +(MAX_Y / 2.0f);
+        tri.v[v].z = g_modelScale * _obj->attrib.vertices.at(3 * i.vertex_index + 2);// +(MAX_Z / 2.0f);
+        if (g_isOffsetting)
+        {
+          tri.v[v] += Hoops::Vec3(MAX_X*0.5f, MAX_Y*0.5f, MAX_Z*0.5f);
+        }
       }
       //Calculating normal once per-triangle
       //rather than once per-ray
@@ -484,21 +566,16 @@ void TriBVTrace(std::vector<unsigned char> &_imageVec, Obj *_obj)
           if (isHit && hitPoint.x < MAX_X && hitPoint.y < MAX_Y && hitPoint.z < MAX_Z
                     && hitPoint.x >= 0  && hitPoint.y >= 0  && hitPoint.z >= 0)
           {
+            if (g_hitsDump) fprintf(g_hitsDump, "0,%i,%i,%f,%f,%f\n", y, x, hitPoint.x, hitPoint.y, hitPoint.z);
+
             if (g_visualDrawing)
             {
               float facingRatio = Hoops::clamp(-Hoops::Dot(CONST_DIR, tri.n), 0.4f, 1.0f) * 0.5f;
 
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 0] = facingRatio * 128;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 1] = facingRatio * 255;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 2] = facingRatio * 128;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 3] = 255;
-            }
-            else
-            {
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 0] = 255;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 1] = 255;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 2] = 255;
-              _imageVec[(MAX_Y - y) *(MAX_Y * 4) + 4 * x + 3] = 255;
+              _imageVec[Hoops::clamp((MAX_Y - y) *(MAX_Y * 4) + 4 * x, 0, (MAX_X*MAX_Y * 4) - 4) + 0] = facingRatio * 128;
+              _imageVec[Hoops::clamp((MAX_Y - y) *(MAX_Y * 4) + 4 * x, 0, (MAX_X*MAX_Y * 4) - 4) + 1] = facingRatio * 255;
+              _imageVec[Hoops::clamp((MAX_Y - y) *(MAX_Y * 4) + 4 * x, 0, (MAX_X*MAX_Y * 4) - 4) + 2] = facingRatio * 128;
+              _imageVec[Hoops::clamp((MAX_Y - y) *(MAX_Y * 4) + 4 * x, 0, (MAX_X*MAX_Y * 4) - 4) + 3] = 255;
             }
           }
         }
